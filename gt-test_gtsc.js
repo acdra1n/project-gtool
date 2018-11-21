e65297794f074bc8114177e132741f2ca0b4737d
mr_chainman <rvreman7@me.com>
2018-11-21 23:41:10 +0400

    fix gt script parser

--- gt-test_gtsc.js
+++ gt-test_gtsc.js
@@ -1,7 +1,4 @@
 var gtp = require("./lib/gtsc/gtscriptparser");
 gtp.context.addVariable("youtube_memes", "pewdiepie");
-
-gtp.parse(`
-#Comment here
-
-`.split('\n'), null);
+var fs = require('fs');
+gtp.parse(fs.readFileSync("gt-test_gtsc_script.gtsc", "utf8").split('\n'), null);
