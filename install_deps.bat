deed88f444fd73970f002b2218b1ee32c37acb9f
mr_chainman <rvreman7@me.com>
2018-11-21 23:06:10 +0400

    Dependency update

--- install_deps.bat
+++ install_deps.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Installing dependencies for project-gtool...
 ::TODO add version specifier for web3
-npm install web3
+npm install web3 math-expression-evaluator
 npm install pkg npm-bundle -g
