35047f8cc9c580159973b7556cee0da16210c37c
mr_chainman <rvreman7@me.com>
2018-11-22 12:46:14 +0400

    Addded get-account-txns

--- gt-test_gtsc.js
+++
@@ -1,4 +0,0 @@
-var gtp = require("./lib/gtsc/gtscriptparser");
-gtp.context.addVariable("youtube_memes", "pewdiepie");
-var fs = require('fs');
-gtp.parse(fs.readFileSync("gt-test_gtsc_script.gtsc", "utf8").split('\n'), null);
