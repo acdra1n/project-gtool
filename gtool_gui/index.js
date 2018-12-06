2bdc485549fc693cadf6c8ed452ed5b8a71d8c00
mr_chainman <rvreman7@me.com>
2018-12-06 21:36:50 +0400

    Add about window to UI

--- gtool_gui/index.js
+++ gtool_gui/index.js
@@ -10,6 +10,8 @@ Description: gtool frontend index file
 
 const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 const ui = require("./lib/ui/ui");
+const cp = require('child_process');
+const os = require('os');
 
 let window = null;
 
@@ -29,18 +31,59 @@ app.once('ready', () => {
         type: "submenu",
         submenu: [
             new MenuItem({
-                type: "normal",
-                label: "Test Menu Item",
-            }),
-            new MenuItem({
-                type: "separator"
-            }),
-            new MenuItem({
                 label: "Exit",
                 click: (menuItem, browserWindow, event)=> {
                     process.exit();
                 }
             })
+        ]
+    }));
+
+    menu.append(new MenuItem({
+        label: "Help",
+        type: "submenu",
+        submenu: [
+            new MenuItem({
+                type: "normal",
+                label: "Documentation",
+                click: (menuItem, browserWindow, event)=>{
+                    switch(os.platform())
+                    {
+                        case "win32":
+                            cp.execSync("start http://github.com/techspider/project-gtool/wiki");
+                            break;
+                        case "linux":
+                            cp.execSync("xdg-open http://github.com/techspider/project-gtool/wiki");
+                            break;
+                        case "darwin":
+                            cp.execSync("open http://github.com/techspider/project-gtool/wiki");
+                            break;
+                    }
+                }
+            }),
+            new MenuItem({
+                type: "separator"
+            }),
+            new MenuItem({
+                type: "normal",
+                label: "About project-gtool",
+                click: (menuItem, browserWindow, event)=> {
+                    var aboutWindow = new BrowserWindow({
+                        width: 500,
+                        height: 230,
+                        titleBarStyle: 'hidden',
+                        backgroundColor: '#e0e0e0',
+                        show: false,
+                        maximizable: false,
+                        minimizable: false,
+                        alwaysOnTop: true,
+                        resizable: false,
+                    });
+                    aboutWindow.setMenu(null);
+                    var aui = new ui.GTUserInterface(aboutWindow, "about.html");
+                    aui.load();
+                }
+            }),
         ]
     }));
     
