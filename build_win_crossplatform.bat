8d0fb681bfc9f29b9ec37f19d5567c406a4d76df
mr_chainman (techspider) <rvreman7@me.com>
2018-12-06 08:40:55 -0800

    Add ui to build script for windows

--- build_win_crossplatform.bat
+++ build_win_crossplatform.bat
@@ -1,7 +1,9 @@
 @echo off
 
 echo Build started [%date%]
+if /i "%1"=="/uionly" goto build_uionly
 
+:build_full
 if not exist build mkdir build
 if not exist build\linux mkdir build\linux
 if not exist build\macos mkdir build\macos
@@ -17,6 +19,10 @@ copy /y .\LICENSE.MD build\win\LICENSE.MD > nul
 copy /y .\LICENSE.MD build\macos\LICENSE.MD > nul
 copy /y .\LICENSE.MD build\linux\LICENSE.MD > nul
 
+echo [gtool]
+echo Targets: node8-win, node8-macos, node8-linux
+echo.
+
 echo Building binary for Windows...
 call pkg -t node8-win index.js -o build\win\gtool.exe > build\build_pkg_win.log
 
@@ -25,3 +31,10 @@ call pkg -t node8-macos index.js -o build\macos\gtool > build\build_pkg_macos.lo
 
 echo Building binary for Linux...
 call pkg -t node8-linux index.js -o build\linux\gtool > build\build_pkg_linux.log
+echo.
+
+:build_uionly
+
+echo [gtool-ui]
+echo Building gtool-ui binary for Windows...
+call node platform\windows\gtool-ui\build.js
